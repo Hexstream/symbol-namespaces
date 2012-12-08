97a3dc3520c15ad2b468ee611c21c50fe8546900
Jean-Philippe Paradis <hexstream@gmail.com>
2012-12-07 22:04:19 -0500

    Add support for in-place redefinition.

--- main.lisp
+++ main.lisp
@@ -5,6 +5,17 @@
 
 (defclass symspace:namespace () ())
 
+(defgeneric symspace:name (namespace))
+
+(defclass symspace:name-mixin ()
+  ((%name :initarg :name
+          :reader symspace:name
+          :type symbol)))
+
+(defmethod print-object ((mixin name-mixin) stream)
+  (print-unreadable-object (mixin stream :type t)
+    (prin1 (symspace:name mixin) stream)))
+
 (defclass symspace:packages-mixin ()
   ())
 
@@ -13,8 +24,8 @@
   (:method ((symbol symbol) (symbol-namespace packages-mixin))
     (let ((package (symbol-package symbol)))
       (if package
-          (intern (symbol-name symbol)
-                  (intern-package package symbol-namespace))
+          (identity (intern (symbol-name symbol)
+                            (intern-package package symbol-namespace)))
           (error "Don't know how to intern ~S in ~S ~
                   as the symbol has no home package."
                  symbol symbol-namespace)))))
@@ -41,7 +52,8 @@
   ((%packages :reader %packages
               :initform (make-hash-table :test 'equal))))
 
-(defclass symspace:standard-namespace (symspace:packages-identity-mixin
+(defclass symspace:standard-namespace (symspace:name-mixin
+                                       symspace:packages-identity-mixin
                                        symspace:namespace)
   ())
 
@@ -53,41 +65,46 @@
               (make-package (symspace:make-package-name package
                                                         symbol-namespace))))))
 
+(defun %call-with-package-name (function package)
+  (let ((package-name (package-name package)))
+    (if package-name
+        (funcall function package-name)
+        (error "Package ~S has been deleted and thus has no name."
+               package))))
+
 (defgeneric symspace:find-package (package symbol-namespace)
   (:argument-precedence-order symbol-namespace package)
   (:method ((package package) (symbol-namespace symspace:packages-identity-mixin))
     (identity (gethash package (%packages symbol-namespace))))
   (:method ((package package) (symbol-namespace symspace:packages-name-mixin))
-    (let ((package-name (package-name package)))
-      (if package-name
-          (identity (gethash package-name (%packages symbol-namespace)))
-          (error "Package ~S has been deleted and thus has no name."
-                 package)))))
+    (%call-with-package-name
+     (lambda (package-name)
+       (identity (gethash package-name (%packages symbol-namespace))))
+     package)))
 
 (defgeneric (setf symspace:find-package) (new package symbol-namespace)
   (:argument-precedence-order new symbol-namespace package)
   (:method (new (package package) (symbol-namespace symspace:packages-identity-mixin))
     (setf (gethash package (%packages symbol-namespace)) new))
   (:method (new (package package) (symbol-namespace symspace:packages-name-mixin))
-    (let ((package-name (package-name package)))
-      (if package-name
-          (setf (gethash package-name (%packages symbol-namespace)) new)
-          (error "Package ~S has been deleted and thus has no name."
-                 package)))))
-
-(defvar *counter* 0)
+    (%call-with-package-name
+     (lambda (package-name)
+       (setf (gethash package-name (%packages symbol-namespace)) new))
+     package)))
 
 (defgeneric symspace:make-package-name (package symbol-namespace)
   (:argument-precedence-order symbol-namespace package)
   (:method ((package package) (symbol-namespace symspace:namespace))
-    (format nil "TEST-~A" (incf *counter*))))
-
-
-(defmacro symspace:define (name &rest options)
-  (declare (ignore options))
-  (check-type name symbol)
-  `(setf (symspace:locate ',name)
-         (make-instance 'symspace:standard-namespace :name ',name)))
+    (%call-with-package-name
+     (lambda (package-name)
+       (let ((namespace-name (symspace:name symbol-namespace)))
+         (format nil "~A_+_~A_+_~A_+_~A"
+                 '#:symspace
+                 (%call-with-package-name #'identity
+                                          (symbol-package namespace-name))
+                 (symbol-name namespace-name)
+                 package-name)))
+     package)))
 
 (defun symspace:locate (symbol &key (errorp t))
   (check-type symbol symbol)
@@ -98,7 +115,45 @@
 
 (defun (setf symspace:locate) (new symbol &key (errorp t))
   (declare (ignore errorp))
-  (check-type symbol symbol)
+  (check-type symbol (and symbol (not null)))
   (check-type new symspace:namespace)
   (setf (gethash symbol *namespaces*)
         new))
+
+(defun %remove-keys (keys plist)
+  (do (acc
+       (plist plist (cddr plist)))
+      ((endp plist) (nreverse acc))
+    (destructuring-bind (key value &rest rest) plist
+      (declare (ignore rest))
+      (unless (member key keys)
+        (setf acc (list* value key acc))))))
+
+(defun symspace:ensure (name &rest keys
+                        &key (class 'symspace:standard-namespace))
+  (setf keys (%remove-keys '(:class) keys))
+  (let ((existing (symspace:locate name :errorp nil)))
+    (cond (existing
+           (setf class (etypecase class
+                         (class class)
+                         (symbol (find-class class))))
+           (if (eq class (class-of existing))
+               (apply #'reinitialize-instance existing keys)
+               (apply #'change-class existing class keys)))
+          (t (setf (symspace:locate name)
+                   (make-instance class :name name))))))
+
+(defmacro symspace:define (name &body options)
+  (check-type name symbol)
+  (let ((plist
+         (let (classp)
+           (map-bind (mapcan) ((option options))
+             (destructuring-bind (kind &rest values) option
+               (if (eq kind :class)
+                   (destructuring-bind (supplied-class) values
+                     (prog1 (list :class `',supplied-class)
+                       (if classp
+                           (error "Duplicate :class option in ~S." options)
+                           (setf classp t))))
+                   (list `',kind `',values)))))))
+    `(symspace:ensure ',name ,@plist)))
--- package.lisp
+++ package.lisp
@@ -3,19 +3,24 @@
               #:symspaces
               #:symspace)
   (:use #:cl)
+  (:import-from #:map-bind #:map-bind)
   (:shadow #:find-package
            #:find-symbol)
   (:export #:namespace
+           #:name-mixin
+           #:name
            #:packages-mixin
            #:packages-identity-mixin
            #:packages-name-mixin
            #:standard-namespace
-           #:define
-           #:locate
 
            #:intern-package
            #:find-package
            #:make-package-name
 
            #:intern-symbol
-           #:find-symbol))
+           #:find-symbol
+
+           #:locate
+           #:ensure
+           #:define))
